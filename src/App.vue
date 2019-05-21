1c4c57435f96bdd43c1b16502dd81811701f2fdc
Erick Dos Santos <erick.dossantos@osf-commerce.com>
2019-05-21 17:53:32 -0300

    Add and Apply ESLint Rules

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to the practical exercises Vue.js App"/>
   </div>
 </template>
 
@@ -9,10 +8,10 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        HelloWorld
+    }
 }
 </script>
 
