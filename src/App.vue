1d765a044e501aa6b9a7dd7b600992a9a3b3370b
Erick Dos Santos <erickluang@gmail.com>
2019-05-21 22:01:11 -0300

    Exercise 1 (#1)
    - Add bootstrap.
    - Delete unnecessary HelloWorld.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,50 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to the practical exercises Vue.js App"/>
-  </div>
+    <div class="container mt-5" id="app">
+        <div class="jumbotron">
+            <h1 class="display-4">Erick Dos Santos</h1>
+            <p
+                class="lead"
+            >Practical Exercises Resolution</p>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <nav id="list-exercise" class="list-group">
+                    <a class="list-group-item list-group-item-action" href="#exercise-1">Exercise 1</a>
+                </nav>
+            </div>
+            <div class="col-9">
+                <div
+                    data-spy="scroll"
+                    data-target="#list-exercise"
+                    data-offset="0"
+                    class="scrollspy-exercises"
+                >
+                    <exercise1-vue/>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Exercise1Vue from './components/exercises/Exercise1.vue';
 
 export default {
     name: 'app',
     components: {
-        HelloWorld
+        Exercise1Vue,
     }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    position: relative;
+    height: 100%;
+}
+.scrollspy-exercises {
+    height: 50vh;
+    overflow: auto;
+    position: relative;
 }
 </style>
---
+++ src/components/exercises/Exercise1.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="exercise-1" class="mb-5 p-4 border">
+        <div class="col-11 border-bottom my-2">
+            <h4>Exercise 1</h4>
+        </div>
+        <div class="col-11 my-2 py-2">
+            <p>{{ message }} - {{ name }} ({{ age }})</p>
+        </div>
+        <div class="col-11 my-2 py-2">
+            <p>Age * 3</p>
+            <code>{{ age }} * 3 = {{ age * 3 }}</code>
+        </div>
+        <div class="col-11 my-2 py-2">
+            <h6>{{ outputRandom() }}</h6>
+        </div>
+        <div class="col-11 my-2 py-2">
+            <label for="age"><b>Age</b></label>
+            <input id="age" class="form-control" type="number" v-model.number="age" >
+            <label for="name"><b>Name</b></label>
+            <input id="name" class="form-control" type="text" v-model="name" >
+        </div>
+        <div class="col-11 my-2 py-2">
+            <img class="img-fluid" :src="imageURL">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'VueJS is pretty cool',
+            name: 'Erick',
+            age: 23,
+            imageURL: 'https://www.osf-global.com/assets/img/content/logo-big.jpg'
+        };
+    },
+    methods: {
+        outputRandom() {
+            return Math.random();
+        }
+    }
+};
+</script>
