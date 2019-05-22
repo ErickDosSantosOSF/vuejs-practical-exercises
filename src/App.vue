5a6076c492d931e8be8b36ec5b82c2667947f4ab
Erick Dos Santos <45692089+ErickDosSantosOSF@users.noreply.github.com>
2019-05-21 22:17:12 -0300

    Boilerplate (#2)
    - Create the exercises boilerplates.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,11 @@
             <div class="col-3">
                 <nav id="list-exercise" class="list-group">
                     <a class="list-group-item list-group-item-action" href="#exercise-1">Exercise 1</a>
+                    <a class="list-group-item list-group-item-action" href="#exercise-2">Exercise 2</a>
+                    <a class="list-group-item list-group-item-action" href="#exercise-3">Exercise 3</a>
+                    <a class="list-group-item list-group-item-action" href="#exercise-4">Exercise 4</a>
+                    <a class="list-group-item list-group-item-action" href="#exercise-5">Exercise 5</a>
+                    <a class="list-group-item list-group-item-action" href="#exercise-6">Exercise 6</a>
                 </nav>
             </div>
             <div class="col-9">
@@ -20,6 +25,11 @@
                     class="scrollspy-exercises"
                 >
                     <exercise1-vue/>
+                    <exercise2-vue/>
+                    <exercise3-vue/>
+                    <exercise4-vue/>
+                    <exercise5-vue/>
+                    <exercise6-vue/>
                 </div>
             </div>
         </div>
@@ -28,11 +38,21 @@
 
 <script>
 import Exercise1Vue from './components/exercises/Exercise1.vue';
+import Exercise2Vue from './components/exercises/Exercise2.vue';
+import Exercise3Vue from './components/exercises/Exercise3.vue';
+import Exercise4Vue from './components/exercises/Exercise4.vue';
+import Exercise5Vue from './components/exercises/Exercise5.vue';
+import Exercise6Vue from './components/exercises/Exercise6.vue';
 
 export default {
     name: 'app',
     components: {
         Exercise1Vue,
+        Exercise2Vue,
+        Exercise3Vue,
+        Exercise4Vue,
+        Exercise5Vue,
+        Exercise6Vue
     }
 };
 </script>
