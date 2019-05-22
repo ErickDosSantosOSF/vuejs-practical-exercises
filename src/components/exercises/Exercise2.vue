5bdb72c86a92fa9a115c5768613bfcb259b75f06
Erick Dos Santos <erick.dossantos@osf-commerce.com>
2019-05-21 22:15:49 -0300

    Boilerplate
    - Create the exercises boilerplates.

---
+++ src/components/exercises/Exercise2.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="exercise-2" class="mb-5 p-4 border">
+        <div class="col-11 border-bottom my-2">
+            <h4>Exercise 2</h4>
+        </div>
+        <div class="col-11 my-2 py-2">
+            Quis magna Lorem anim amet ipsum do mollit
+            sit cillum voluptate ex nulla tempor.
+            Laborum consequat non elit enim exercitation
+            cillum aliqua consequat id aliqua.
+            Esse ex consectetur mollit voluptate est in duis
+            laboris ad sit ipsum anim Lorem. Incididunt veniam
+            velit elit elit veniam Lorem aliqua quis ullamco
+            deserunt sit enim elit aliqua esse irure.
+            Laborum nisi sit est tempor laborum mollit labore
+            officia laborum excepteur commodo non commodo dolor
+            excepteur commodo. Ipsum fugiat ex est consectetur ipsum commodo tempor sunt in proident.
+        </div>
+        <div class="col-11 my-2 py-2"></div>
+    </div>
+</template>
